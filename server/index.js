40ec02c5bff07801b66e227babf4a362224a886b
Soorria Saruva <soorria.ss@gmail.com>
2020-09-20 12:52:41 +1000

    Remove unused variable

--- server/index.js
+++ server/index.js
@@ -1,137 +1,135 @@
-const express = require("express");
-const { createServer } = require("http");
-const Server = require("socket.io");
-const dotenv = require("dotenv");
-const volleyball = require("volleyball");
-const cors = require("cors");
-const helmet = require("helmet");
-const cookieParser = require("cookie-parser");
+const express = require('express')
+const { createServer } = require('http')
+const Server = require('socket.io')
+const dotenv = require('dotenv')
+const volleyball = require('volleyball')
+const cors = require('cors')
+const helmet = require('helmet')
+const cookieParser = require('cookie-parser')
 
-const connectToDB = require("./db");
-const allRoutes = require("./routes");
-const notFound = require("./middlewares/notFound");
-const errorHandler = require("./middlewares/errorHandler");
-const { verifyToken } = require("./utils/jwt");
-const Chat = require("./models/Chat");
-const { createChat } = require("./controllers/chat");
+const connectToDB = require('./db')
+const allRoutes = require('./routes')
+const notFound = require('./middlewares/notFound')
+const errorHandler = require('./middlewares/errorHandler')
+const { verifyToken } = require('./utils/jwt')
+const Chat = require('./models/Chat')
+const { createChat } = require('./controllers/chat')
 
-dotenv.config();
+dotenv.config()
 
-const userIdToSocket = new Map();
+const userIdToSocket = new Map()
 
-const origin = process.env.WEB_URL || "http://localhost:3000";
-
-(async () => {
-  const app = express();
-  const http = createServer(app);
+;(async () => {
+  const app = express()
+  const http = createServer(app)
 
   // JSON Body parser
-  app.use(express.json());
+  app.use(express.json())
   // Cookie Parser
-  app.use(cookieParser());
+  app.use(cookieParser())
 
   // Logger
-  app.use(volleyball);
+  app.use(volleyball)
 
   // Header stuff
-  app.use(helmet());
+  app.use(helmet())
   app.use(
     cors({
       origin: true,
       credentials: true,
     })
-  );
+  )
 
   // Add routes
-  app.use(allRoutes);
+  app.use(allRoutes)
 
   // Connect to MongoDB with mongoose
-  await connectToDB();
+  await connectToDB()
 
-  const io = new Server(http, { origins: "*:*", cookie: false });
+  const io = new Server(http, { origins: '*:*', cookie: false })
 
   // Socket connection
-  io.on("connection", async (socket) => {
-    const jwt = socket.handshake.query.token;
-    console.log(1);
+  io.on('connection', async socket => {
+    const jwt = socket.handshake.query.token
+    console.log(1)
 
-    let userId;
+    let userId
 
     try {
-      userId = verifyToken(jwt).id;
-      console.log(2);
+      userId = verifyToken(jwt).id
+      console.log(2)
     } catch (err) {
-      console.log(3);
-      socket.disconnect(true);
-      return;
+      console.log(3)
+      socket.disconnect(true)
+      return
     }
 
-    socket.on("disconnect", () => {
-      console.log(4, jwt, userId);
-      userIdToSocket.delete(userId);
-    });
+    socket.on('disconnect', () => {
+      console.log(4, jwt, userId)
+      userIdToSocket.delete(userId)
+    })
 
-    userIdToSocket.set(userId, socket);
+    userIdToSocket.set(userId, socket)
 
     const userChats = await Chat.find({
       members: { $elemMatch: { $eq: userId } },
-    });
+    })
 
-    userChats.forEach((c) => {
-      socket.join(c._id);
-    });
+    userChats.forEach(c => {
+      socket.join(c._id)
+    })
 
-    socket.on("chat message", async ({ chatId, message }) => {
-      if (!message) return;
+    socket.on('chat message', async ({ chatId, message }) => {
+      if (!message) return
       try {
-        const targetChat = await Chat.findById(chatId);
+        const targetChat = await Chat.findById(chatId)
         const newMessage = {
           body: message,
           sender: userId,
           date: Date.now(),
-        };
-        targetChat.messages.push(newMessage);
-        await targetChat.save();
-        io.to(chatId).emit("chat message", chatId, newMessage);
+        }
+        targetChat.messages.push(newMessage)
+        await targetChat.save()
+        io.to(chatId).emit('chat message', chatId, newMessage)
       } catch (err) {
-        console.log(err);
-        io.to(socket.id).emit("error", {
+        console.log(err)
+        io.to(socket.id).emit('error', {
           message:
-            "An error occurred while sending your message. Please try again later.",
-        });
+            'An error occurred while sending your message. Please try again later.',
+        })
       }
-    });
+    })
 
-    socket.on("create chat", async ({ name, members }) => {
-      if (!name || new Set(members).size < 2) return;
+    socket.on('create chat', async ({ name, members }) => {
+      if (!name || new Set(members).size < 2) return
       try {
-        const newChat = await createChat(members, name);
-        socket.join(newChat._id);
+        const newChat = await createChat(members, name)
+        socket.join(newChat._id)
         newChat.members.forEach(({ _id: id }) => {
           if (userIdToSocket.has(id.toString()) && id != userId) {
-            console.log("connected new member");
-            userIdToSocket.get(id.toString()).join(newChat._id);
+            console.log('connected new member')
+            userIdToSocket.get(id.toString()).join(newChat._id)
           }
-        });
-        io.to(newChat._id).emit("create chat", newChat);
+        })
+        io.to(newChat._id).emit('create chat', newChat)
       } catch (err) {
-        console.log(err);
-        io.to(socket.id).emit("error", {
+        console.log(err)
+        io.to(socket.id).emit('error', {
           message:
-            "An error occurred while creating your chat. Please try again later.",
-        });
+            'An error occurred while creating your chat. Please try again later.',
+        })
       }
-    });
-  });
+    })
+  })
 
   // Default route handler
-  app.use(notFound);
+  app.use(notFound)
 
   // Default error handler
-  app.use(errorHandler);
+  app.use(errorHandler)
 
-  const port = process.env.PORT || 1234;
+  const port = process.env.PORT || 1234
   http.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
-})();
+    console.log(`Server listening on port ${port}`)
+  })
+})()
